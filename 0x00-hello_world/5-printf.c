17e2b890abc9cf92076db75d2330a76f1914c7e9
Camilo Araque <camiaraquecaro17@gmail.com>
2019-09-18 20:43:16 +0000

    fixed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  * main - Prints out a string to stdout.
- * This is Exercise five
+ * Return: 0 if success.
  */
-void main(void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
