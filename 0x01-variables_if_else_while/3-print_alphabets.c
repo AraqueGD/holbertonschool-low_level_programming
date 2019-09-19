5f926928f324fb212c58b08647e6ac1982bd0eb9
Camilo Araque <camiaraquecaro17@gmail.com>
2019-09-19 21:31:19 +0000

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int x = 'a';
+while (x <= 'z')
+{
+putchar(x);
+x++;
+}
+x = 'A';
+while (x <= 'Z')
+{
+putchar(x);
+x++;
+}
+putchar('\n');
+return (0);
+}
