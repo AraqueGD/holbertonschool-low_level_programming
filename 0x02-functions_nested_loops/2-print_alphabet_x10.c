beac631ef1f104e0df49edca5b3f5e5788e3bc83
Camilo Araque <camiaraquecaro17@gmail.com>
2019-09-24 16:56:09 +0000

    Fixed

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,18 +8,18 @@
 
 void print_alphabet_x10(void)
 {
-	int i = 0;
-	char c;
+int i = 0;
+char c;
 
-	while (i < 10)
+while (i < 10)
 {
-	c = 'a';
-	while (c < 'z')
+c = 'a';
+while (c < 'z')
 {
-	_putchar(c);
-		c++;
+_putchar(c);
+c++;
 }
-	_putchar('\n');
-	i++;
+_putchar('\n');
+i++;
 }
 }
