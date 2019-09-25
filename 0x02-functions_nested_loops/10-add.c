932ab9925864971026237e00dd1676c19c07cc1b
Camilo Araque <camiaraquecaro17@gmail.com>
2019-09-25 03:42:44 +0000

    10. a + b - Write a function that adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * add - check the code for Holberton School students.
+ * @x: @y: 98
+ * Return: rta.
+ */
+
+int add(int x, int y)
+{
+	int rta = 0;
+	rta = x + y;
+	return (rta);
+}
