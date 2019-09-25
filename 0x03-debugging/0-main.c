2b2a097e263dd1105fde595281e5bc4cec2fc519
Camilo Araque <camiaraquecaro17@gmail.com>
2019-09-25 18:19:19 +0000

    0. Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * positive_or_negative - tests function that prints if integer.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
