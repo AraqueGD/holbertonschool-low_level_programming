76cc5192376d3b35af4c45bd5f31af761a71ba9e
Camilo Araque <camiaraquecaro17@gmail.com>
2019-09-26 16:21:25 +0000

    2. Collaboration is multiplication - Write a function that multiplies two integers.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * mul - check the code for Holberton School students.
+ * @a: Parameter one.
+ * @b: Parameter two.
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	int mul = 0;
+
+	mul = a * b;
+	return (mul);
+}
