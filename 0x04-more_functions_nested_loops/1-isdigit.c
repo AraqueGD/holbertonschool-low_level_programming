1ce94ecd70c90446a13b3ff64159b3e75c15616f
Camilo Araque <camiaraquecaro17@gmail.com>
2019-09-26 15:53:11 +0000

    1. isdigit - Write a function that checks for a digit (0 through 9).

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - check the code for Holberton School students.
+ * @c: Parameter
+ * Return: Always 0.
+ */
+
+int _isdigit(int c)
+{
+
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
