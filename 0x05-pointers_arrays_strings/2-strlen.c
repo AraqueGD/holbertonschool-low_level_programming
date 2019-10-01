640f75407eb5979364c8a1004596b48d788bd240
Camilo Araque <camiaraquecaro17@gmail.com>
2019-10-01 06:34:43 +0000

    2. This report, by its very length, defends itself against the risk of being read - Write a function that returns the length of a string.

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strlen - check the code for Holberton School students.
+ * @s: Parameter
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	char *str;
+	int num = 0;
+	str = s;
+	while (*str != '\0')
+{
+	num++;
+	str++;
+}
+	return (num);
+}
