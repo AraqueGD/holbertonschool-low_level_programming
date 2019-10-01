7ffa3c2b647000e4dcf214aa9b16efd5c3ddbde5
Camilo Araque <camiaraquecaro17@gmail.com>
2019-10-01 21:21:26 +0000

    6. Half the lies they tell about me aren't true - Write a function that prints every other character of a string, starting with the first character, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * puts2 - check the code for Holberton School students.
+ * @str: Parameter
+ * Return: nothing.
+ */
+
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		if (*str % 2 == 0)
+		{
+			_putchar(*str);
+		}
+		str++;
+	}
+	_putchar('\n');
+}
