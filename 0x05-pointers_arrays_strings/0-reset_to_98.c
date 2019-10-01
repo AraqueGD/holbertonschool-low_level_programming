d77c2abded04eb169c75f76c36a5486b8f24fb2a
Camilo Araque <camiaraquecaro17@gmail.com>
2019-10-01 16:00:37 +0000

    Fixed checker 6 and add 1. Don't swap horses in crossing a stream

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * reset_to_98 - check the code for Holberton School students.
+ * @n: Parameter
+ * Return: Nothing.
  */
 
 void reset_to_98(int *n)
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * swap_int - check the code for Holberton School students.
+ *
+ * @a: First Parameter
+ * @b: Second Parameter
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
