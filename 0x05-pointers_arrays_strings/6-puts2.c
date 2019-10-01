c4568f7f423b0215c79729955d271b0765da31ac
Camilo Araque <camiaraquecaro17@gmail.com>
2019-10-01 21:36:14 +0000

    Fixed

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,12 +2,14 @@
 
 /**
  * puts2 - check the code for Holberton School students.
+ *
  * @str: Parameter
  * Return: nothing.
  */
 
 void puts2(char *str)
 {
+
 	while (*str != '\0')
 	{
 		if (*str % 2 == 0)
