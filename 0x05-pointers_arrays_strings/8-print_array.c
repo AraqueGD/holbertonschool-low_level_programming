08890e26cc17b57e4ddb0d8d1b130d2c3636d499
Camilo Araque <camiaraquecaro17@gmail.com>
2019-10-02 19:11:05 +0000

    8. Arrays are not pointers - Write a function that prints n elements of an array of integers, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - check the code for Holberton School students.
+ * @a: First Parameter
+ * @n: Second Parameter
+ * Return: Nothing.
+ */
+
+void print_array(int *a, int n)
+{
+	int count = 0;
+
+	if (n > 0)
+	{
+		while (count < n)
+		{
+			printf("%d", a[count]);
+			if (count < n -1)
+				printf(", ");
+			count++;
+		}
+	}
+		printf("\n");
+}
