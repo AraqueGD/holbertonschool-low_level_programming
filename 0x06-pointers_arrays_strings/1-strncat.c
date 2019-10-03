a48a9b1f398ef97869b3fa02fb7dab1d125e73be
Camilo Araque <camiaraquecaro17@gmail.com>
2019-10-03 16:05:39 +0000

    1. strncat - Write a function that concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * _strcat - check the code for Holberton School students.
+ * @dest: First Parameter
+ * @src: Second Parameter
+ * Return: Always 0.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	unsigned int i = 0;
+        int j = 0;
+
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
+	while (*(src + j) != '\0')
+	{
+		if ( j < n)
+		{
+		*(dest + i) = *(src + j);
+		i++;
+		}
+		j++;
+	}
+	return (dest);
+}
