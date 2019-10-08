267a2262d80fe6e9e8c73753c67e8f75428d2279
Camilo Araque <camiaraquecaro17@gmail.com>
2019-10-08 03:21:08 +0000

    Ggg!

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,26 +10,13 @@
 
 char *_strchr(char *s, char c)
 {
-	int flag = 0;
-	char *ptrS = s;
-	int idx;
-	char *ptrAux = s;
 
-	for (idx = 0; ptrS[idx] != '\0'; idx++)
+	for (; *s != '\0'; s++)
 	{
-		if (ptrS[idx] == c)
+		if (*s == c)
 		{
-			flag = 1;
-			break;
+			return (s);
 		}
-		ptrAux++;
 	}
-	if (flag == 0)
-	{
-		return (NULL);
-	}
-	else
-	{
-		return (ptrAux);
-	}
+	return (NULL);
 }
