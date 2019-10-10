107d54464ea14006d3a00c48e9419376dbee5dc9
Camilo Araque <camiaraquecaro17@gmail.com>
2019-10-10 02:43:25 +0000

    2. Dreams feel real while we're in them. It's only when we wake up that we realize something was actually strange - Write a function that returns the length of a string.

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - check the code for Holberton School students.
+ * @s: Pointer to strlen.
+ * Return: Always 0.
+ */
+
+int _strlen_recursion(char *s)
+{
+	if(*s)
+	{
+		int len =_strlen_recursion(s + 1);
+		return (++len);
+	}
+	return (0);
+}
