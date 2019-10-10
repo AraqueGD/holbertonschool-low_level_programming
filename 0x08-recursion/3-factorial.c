3e1d26ce70d6fd8d182c8f4077d942ef6ed808e6
Camilo Araque <camiaraquecaro17@gmail.com>
2019-10-10 03:18:49 +0000

    3. You mustn't be afraid to dream a little bigger, darling - Write a function that returns the factorial of a given number.

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * factorial - check the code for Holberton School students.
+ * @n: Factories.
+ * Return: Always 0.
+ */
+
+int factorial(int n)
+{
+	if (n == 1 || n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+		return (n * factorial(n - 1));
+}
