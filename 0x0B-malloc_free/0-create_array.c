3cdec93edb3ad2e0ee07c0aab79744a948a6cd8f
Camilo Araque <camiaraquecaro17@gmail.com>
2019-10-16 02:01:33 +0000

    0. Float like a butterfly, sting like a bee - Write a function that creates an array of chars, and initializes it with a specific char.

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @c: String.
+ * @size: the size of the memory to print.
+ * Return: Nothing.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i = 0;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(sizeof(*ptr) * size);
+	if(ptr == NULL)
+	{
+		return (NULL);
+	}
+	while(i <= size - 1)
+	{
+		ptr[i] = c;
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
