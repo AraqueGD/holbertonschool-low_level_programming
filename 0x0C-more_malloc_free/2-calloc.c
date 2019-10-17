5ac795e522099a970b3687fafeca60d871d6ddde
Camilo Araque <camiaraquecaro17@gmail.com>
2019-10-17 21:02:35 +0000

    fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -30,3 +30,25 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	ptr[i] = '\0';
 	return (ptr);
 }
+#include "holberton.h"
+
+/**
+ * *_memset - check the code for Holberton School students.
+ * @s: Starting address of memory to be filled.
+ * @b: Value to be filled.
+ * @n: Number of bytes to be filled starting from ptr to be filled.
+ * Return: Char.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int idx = 0;
+	char *a = s;
+
+	while (idx < n)
+	{
+		a[idx] = b;
+		idx++;
+	}
+	return (a);
+}
