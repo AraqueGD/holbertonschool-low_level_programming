de36d23eab607fbe8bcb546d39c47bf44c94fb6c
Camilo Araque <camiaraquecaro17@gmail.com>
2019-10-26 20:04:11 +0000

    Fix check 10

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -22,7 +22,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		for (i = 0; i < n; i++)
 		{
 			aux = va_arg(arguments, int);
-			if (i == n - 1)
+			if (separator == NULL)
+				printf("%s", separator);
+			else if (i == n - 1)
 				printf("%d", aux);
 			else
 				printf("%d%s", aux, separator);
