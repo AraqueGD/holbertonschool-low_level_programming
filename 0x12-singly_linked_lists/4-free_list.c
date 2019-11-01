814a2582be93332b710124b4295536f51ba67886
Camilo Araque <camiaraquecaro17@gmail.com>
2019-11-01 23:04:42 +0000

    [ADD]4. Free list and [UPD]Header Prototyoe

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list_t list
+ * @head: input header pointer
+ * Return: nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *aux;
+
+	if (head != NULL)
+	{
+		aux = head;
+		head = aux->next;
+		free(aux->str);
+		free(aux);
+	}
+}
