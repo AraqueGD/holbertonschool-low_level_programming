6ad98b27ae37e60c01fc9814c537f256f7c0fbed
Camilo Araque <camiaraquecaro17@gmail.com>
2019-11-01 22:51:55 +0000

    [ADD] 3. Add node at the end AND [UPD]Header Prototype

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+#include <string.h>
+
+/**
+ * add_node_end - function that prints all the elements of a list_t list
+ * @head: input header pointer.
+ * @str: Input string value.
+ * Return: address of the new element, or NULL if it failed
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *add_new;
+	list_t *aux;
+
+	add_new = malloc(sizeof(list_t));
+	if (add_new == NULL)
+	{
+		return (NULL);
+	}
+	add_new->len = _strlen(str);
+	add_new->str = strdup(str);
+	add_new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = add_new;
+	}
+	else
+	{
+		aux = *head;
+		while (aux->next != NULL)
+		{
+			aux = aux->next;
+		}
+		aux->next = add_new;
+	}
+	return (add_new);
+}
+
+/**
+ * _strlen - check the code for Holberton School students.
+ * @s: Parameter.
+ * Return: Always 0.
+ */
+
+int _strlen(const char *s)
+{
+	const char *str_node;
+	int num = 0;
+
+	str_node = s;
+	while (*str_node != '\0')
+	{
+		num++;
+		str_node++;
+	}
+	return (num);
+}
