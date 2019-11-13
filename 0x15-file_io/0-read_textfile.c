f8c7b0cbdce1d77ef73185584b9d337bee19850c
Camilo Araque <camiaraquecaro17@gmail.com>
2019-11-13 05:37:18 +0000

    Fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,6 +11,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t _bytes, fd, idx = 0;
 	char *buff = NULL;
+	const int n = 1;
 
 	if (filename == NULL)
 	{
@@ -26,12 +27,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-	_bytes = read(fd, &buff[idx], 1);
+	_bytes = read(fd, &buff[idx], n);
 	while (((size_t)idx <= letters - 1) && (_bytes != 0))
 	{
-		write(STDOUT_FILENO, &buff[idx], 1);
+		write(STDOUT_FILENO, &buff[idx], _bytes);
 		idx++;
-		_bytes = read(fd, &buff[idx], 1);
+		_bytes = read(fd, &buff[idx], n);
 	}
 	close(fd);
 	free(buff);
