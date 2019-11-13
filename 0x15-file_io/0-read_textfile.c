2430b1b44f2be95126d188e583bfc238fbc21f95
Camilo Araque <camiaraquecaro17@gmail.com>
2019-11-13 05:33:00 +0000

    FIXED

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,32 +9,31 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  ssize_t _bytes, fd, idx = 0;
-  char *buff = NULL;
+	ssize_t _bytes, fd, idx = 0;
+	char *buff = NULL;
 
-  if (filename == NULL)
-  {
-    return (0);
-  }
-  fd = open(filename, O_RDONLY);
-  if (fd == -1)
-  {
-    return (0);
-  }
-  buff = malloc(sizeof(char) * letters);
-  if (buff == NULL)
-  {
-    return (0);
-  }
-  _bytes = read(fd, &buff[idx], 1);
-  while (((size_t)idx <= letters - 1) && (_bytes != 0))
-  {
-    write(STDOUT_FILENO, &buff[idx], 1);
-    idx++;
-    _bytes = read(fd, &buff[idx], 1);
-  }
-  close(fd);
-  free(buff);
-
-  return (idx);
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+	{
+		return (0);
+	}
+	_bytes = read(fd, &buff[idx], 1);
+	while (((size_t)idx <= letters - 1) && (_bytes != 0))
+	{
+		write(STDOUT_FILENO, &buff[idx], 1);
+		idx++;
+		_bytes = read(fd, &buff[idx], 1);
+	}
+	close(fd);
+	free(buff);
+	return (idx);
 }
