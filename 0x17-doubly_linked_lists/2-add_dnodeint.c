e69b3a224a3b802d400de8f79060ffa317e7c676
AraqueGD <camiaraquecaro17@gmail.com>
2019-12-15 14:00:08 -0500

    [ADD]2. Add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,34 +1,36 @@
 #include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * *add_dnodeint - Function Add Begin Nodes.
+ * @head: Parameter Copy Head Node
+ * @n: Parameter the Data Node.
  * Return: Always EXIT_SUCCESS.
  */
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *Newnode = NULL;
-    Newnode = malloc(sizeof(dlistint_t));
-    if (Newnode == NULL)
-    {
-        printf("No se pudo Asignar Memoria...");
-        return (NULL);
-    }
-    if ((*head) == NULL)
-    {
-        Newnode->n = n;
-        Newnode->next = NULL;
-        Newnode->prev = NULL;
-        (*head) = Newnode;
-    }
-    else
-    {
-        Newnode->n = n;
-        Newnode->next = *head;
-        Newnode->prev = NULL;
-        (*head)->prev = Newnode;
-        *head = Newnode;
-    }
-    return (Newnode);
+dlistint_t *Newnode = NULL;
+
+Newnode = malloc(sizeof(dlistint_t));
+if (Newnode == NULL)
+{
+printf("No se pudo Asignar Memoria...");
+return (NULL);
+}
+if ((*head) == NULL)
+{
+Newnode->n = n;
+Newnode->next = NULL;
+Newnode->prev = NULL;
+(*head) = Newnode;
+}
+else
+{
+Newnode->n = n;
+Newnode->next = *head;
+Newnode->prev = NULL;
+(*head)->prev = Newnode;
+*head = Newnode;
+}
+return (Newnode);
 }
