441cbcab191f9c77cb382b7861a6a9c868f4230e
AraqueGD <camiaraquecaro17@gmail.com>
2019-12-15 13:12:48 -0500

    [ADD]1. List length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - Function Print Number Nodes.
+ * @h: Parameter the *Head.
+ * Return: Number Nodes.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+size_t count = 0;
+
+while (h)
+{
+h = h->next;
+count++;
+}
+return (count);
+}
