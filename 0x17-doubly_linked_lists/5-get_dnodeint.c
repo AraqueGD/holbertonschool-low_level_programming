0c4dc36ad3297d6c224c428367c77377c92717aa
AraqueGD <camiaraquecaro17@gmail.com>
2019-12-15 18:05:30 -0500

    [Fixed]

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,17 +1,19 @@
 #include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * get_dnodeint_at_index - check the code for Holberton School students.
+ * @head: The Parameter Head the Nodes.
+ * @index: The Parameter Number Find node.
  * Return: Always EXIT_SUCCESS.
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-  unsigned int i = 0;
-  while (i < index)
-  {
-    head = head->next;
-    i++;
-  }
-  return (head);
+unsigned int i = 0;
+
+while (i < index)
+{
+head = head->next;
+i++;
+}
+return (head);
 }
