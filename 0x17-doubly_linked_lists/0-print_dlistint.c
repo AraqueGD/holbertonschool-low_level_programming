0fd803f0003f64d41be680faf315be31b4138ae0
AraqueGD <camiaraquecaro17@gmail.com>
2019-12-15 12:42:51 -0500

    [ADD]0. Print list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_dlistint - check the code for Holberton School students.
+ * @h: Parameter the *Head
+ * Return: The Number Nodes.
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+    size_t i = 0;
+    while (h)
+    {
+        printf("%d\n", h->n);
+        h = h->next;
+        i++;
+    }
+    return i;
+}
