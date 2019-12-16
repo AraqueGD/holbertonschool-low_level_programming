73034e3ef4049a56230f2af1023ae3510b20bbfc
AraqueGD <camiaraquecaro17@gmail.com>
2019-12-15 22:22:07 -0500

    FIxed

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,11 +1,14 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - check the code for Holberton School students.
- * @h: Parameter Head Nodes.
- * @idx: The Positon Node.
- * @n: The Data Node.
- * Return: Always EXIT_SUCCESS.
+ * insert_dnodeint_at_index - Inserts a new node in a dlistint_t
+ *                            list at a given position.
+ * @h: A pointer to the head of the dlistint_t list.
+ * @idx: The position to insert the new node.
+ * @n: The integer for the new node to contain.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
@@ -21,18 +24,14 @@ NewNode = (dlistint_t *)malloc(sizeof(dlistint_t));
 if (NewNode == NULL)
 return (NULL);
 
-while (pos < idx)
+while (pTmp->next != NULL && pos < idx)
 {
-if (pTmp->next == NULL)
-return (NULL);
 pTmp = pTmp->next;
 pos++;
 }
-if (pTmp->next == NULL)
-{
-  return (add_dnodeint_end(h,n));
-}
 
+if (pTmp->next == NULL)
+return (add_dnodeint_end(h, n));
 
 NewNode->n = n;
 NewNode->next = NULL;
