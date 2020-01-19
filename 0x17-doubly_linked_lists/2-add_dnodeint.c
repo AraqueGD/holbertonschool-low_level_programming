028a648b85f73b0ef84a8eff3726004b5b3ae044
AraqueGD <camiaraquecaro17@gmail.com>
2020-01-18 21:37:58 -0500

    Fixed Betty

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,28 +9,28 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-dlistint_t *Newnode = NULL;
+  dlistint_t *Newnode = NULL;
 
-Newnode = malloc(sizeof(dlistint_t));
-if (Newnode == NULL)
-{
-printf("No se pudo Asignar Memoria...");
-return (NULL);
-}
-if ((*head) == NULL)
-{
-Newnode->n = n;
-Newnode->next = NULL;
-Newnode->prev = NULL;
-(*head) = Newnode;
-}
-else
-{
-Newnode->n = n;
-Newnode->next = *head;
-Newnode->prev = NULL;
-(*head)->prev = Newnode;
-*head = Newnode;
-}
-return (Newnode);
+  Newnode = malloc(sizeof(dlistint_t));
+  if (Newnode == NULL)
+  {
+    printf("No se pudo Asignar Memoria...");
+    return (NULL);
+  }
+    if ((*head) == NULL)
+  {
+    Newnode->n = n;
+    Newnode->next = NULL;
+    Newnode->prev = NULL;
+    (*head) = Newnode;
+  }
+  else
+  {
+    Newnode->n = n;
+    Newnode->next = *head;
+    Newnode->prev = NULL;
+    (*head)->prev = Newnode;
+    *head = Newnode;
+  }
+  return (Newnode);
 }
--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,29 +8,29 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-dlistint_t *Newnode = NULL;
-dlistint_t *tmp = (*head);
+  dlistint_t *Newnode = NULL;
+  dlistint_t *tmp = (*head);
 
-Newnode = malloc(sizeof(dlistint_t));
-if (Newnode == NULL)
-{
-printf("No se realizo Asignación de Memoria");
-return (NULL);
-}
-Newnode->n = n;
-Newnode->prev = NULL;
-Newnode->next = NULL;
+  Newnode = malloc(sizeof(dlistint_t));
+  if (Newnode == NULL)
+  {
+    printf("No se realizo Asignación de Memoria");
+    return (NULL);
+  }
+    Newnode->n = n;
+    Newnode->prev = NULL;
+    Newnode->next = NULL;
 
-if ((*head) == NULL)
-{
-(*head) = Newnode;
-return (Newnode);
-}
-while (tmp->next)
-{
-tmp = tmp->next;
-}
-tmp->next = Newnode;
-Newnode->prev = tmp;
-return (Newnode);
+  if ((*head) == NULL)
+  {
+    (*head) = Newnode;
+    return (Newnode);
+  }
+  while (tmp->next)
+  {
+    tmp = tmp->next;
+  }
+  tmp->next = Newnode;
+  Newnode->prev = tmp;
+  return (Newnode);
 }
