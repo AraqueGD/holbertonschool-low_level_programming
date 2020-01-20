ebf3de61061b7a472416a7186f2fa3e29f6d1c52
AraqueGD <camiaraquecaro17@gmail.com>
2020-01-19 21:48:28 -0500

    [ADD]5. >>> print(ht)

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - check the code for Holberton School students.
+ * @ht: Hash Table
+ * Return: void.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int idx;
+	hash_node_t *tmp_node = NULL;
+	int flag = 0;
+
+	if (ht == NULL)
+	{
+		return;
+	}
+	printf("{");
+	for (idx = 0; idx < ht->size; idx++)
+	{
+		if (ht->array[idx] != NULL)
+		{
+			tmp_node = ht->array[idx];
+			while (tmp_node != NULL)
+			{
+				if (flag == 1)
+				{
+					printf(", ");
+				}
+				printf("'%s': '%s'", tmp_node->key, tmp_node->value);
+				flag = 0;
+				tmp_node = tmp_node->next;
+			}
+		}
+	}
+	printf("}\n");
+}
