113028a6b9ca3e2bbb3c0a6c11a7a2fdd513420f
AraqueGD <camiaraquecaro17@gmail.com>
2020-01-19 19:30:20 -0500

    [FIx]

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,32 +10,34 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int idx = 0;
-    char *tmp_value = strdup(value);
-    hash_node_t *new_node;
-    if (ht == NULL || key == NULL || value == NULL || *key == '\0')
-    {
-        return (EXIT_FAILURE);
-    }
-    idx = key_index((unsigned char *)key, ht->size);
-    if (ht->array)
-    {
-        ht->array[idx] = malloc(sizeof(hash_node_t));
-        if (ht->array[idx] == NULL)
-        {
-            return (EXIT_FAILURE);
-        }
-        ht->array[idx]->key = strdup(key);
-        ht->array[idx]->value = tmp_value;
-        ht->array[idx]->next = NULL;
-    }
-    else
-    {
-        new_node = malloc(sizeof(hash_node_t));
-        new_node->key = strdup(key);
-        new_node->value = tmp_value;
-        new_node->next = ht->array[idx];
-        ht->array[idx] = new_node;
-    }
-    return(EXIT_SUCCESS);
+	unsigned long int idx = 0;
+	char *tmp_value = strdup(value);
+	hash_node_t *new_node;
+
+	if (ht == NULL || key == NULL || value == NULL || *key == '\0')
+	{
+		return (EXIT_FAILURE);
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		return (EXIT_FAILURE);
+	}
+	idx = key_index((unsigned char *)key, ht->size);
+
+	if (ht->array[idx] != NULL)
+	{
+		if (strcmp(ht->array[idx]->key, key) == 0)
+		{
+			free(new_node);
+			free(ht->array[idx]->value);
+			ht->array[idx]->value = tmp_value;
+			return (EXIT_SUCCESS);
+		}
+	}
+	new_node->key = strdup(key);
+	new_node->value = tmp_value;
+	new_node->next = ht->array[idx];
+	ht->array[idx] = new_node;
+	return (EXIT_SUCCESS);
 }
