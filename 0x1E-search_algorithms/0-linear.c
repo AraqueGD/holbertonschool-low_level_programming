30ff550e879456c258c7107412d87913844890c3
AraqueGD <camiaraquecaro17@gmail.com>
2020-04-16 17:31:18 -0500

    [ADD]0. Linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,31 @@
+#include "search.h"
+#include <stdio.h>
+/**
+ * linear_search - Linear search algorithm
+ *
+ * Return: Int Value Search
+ */
+int linear_search(int *array, size_t size, int value)
+{
+    unsigned int idx = 0;
+
+    if (array == NULL)
+    {
+        return (-1);
+    }
+
+    while (idx < size)
+    {
+        if (array[idx] == value)
+        {
+            printf("Value checked array[%u] = [%d]\n", idx, array[idx]);
+            return (idx);
+        }
+        else
+        {
+            printf("Value checked array[%u] = [%d]\n", idx, array[idx]);
+            idx++;
+        }
+    }
+    return (-1);
+}
